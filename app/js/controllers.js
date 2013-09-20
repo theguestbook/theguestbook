c2bc793e26675114b124130b4de5238c2b70db01
OpenShift guest <molash.michael@gmail.com>
2013-09-20 18:13:44 -0400

    Backend wired in

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,8 +5,8 @@
 angular.module('myApp.controllers', [])
   .controller('PostCtrl', ['$scope', function($scope) {
     $scope.posts = [{
-        source: '192.168.1.1',
-        destination: '192.168.1.1'
+        source: 'I like cats',
+        destination: 'Cats are fun. They really make me feel happy with myself, you know?'
     }, {
         source: '192.168.1.1',
         destination: '192.168.1.1'
