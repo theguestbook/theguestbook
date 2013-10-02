73ef279402f8af0ecdb42bbcd8ba575c1a124f48
Bradley Bain <brad_bain@live.com>
2013-10-01 20:17:56 -0500

    content is now a textarea rather than textbox

--- app/js/directives.js
+++ app/js/directives.js
@@ -31,7 +31,7 @@ angular.module('myApp.directives', [])
 
             $scope.newPost = function() {
                 postalService.newPost($scope.title, $scope.content, function(err) {
-                    if(err) console.log(err);
+                    if(err);
                 });
             };
         }
